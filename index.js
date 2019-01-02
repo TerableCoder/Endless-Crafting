9bcf42e9bf276849fded809393f331b006b316c0
Bubble <33262142+Sunpui@users.noreply.github.com>
2019-01-02 14:33:53 +0100

    bigint+fatigability

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ module.exports = function EndlessCrafting(dispatch) {
 	let craftItem;
 	let pp;
 	let cureId = 182439; //Normal: 181100, elite: 182439
-	let cureDbid = 0;
+	let cureDbid = BigInt(0);
 
 	command.add('craft', (chatLink) => {
 		if (!chatLink) {
@@ -30,7 +30,7 @@ module.exports = function EndlessCrafting(dispatch) {
 			var dbid = chatLink.match(regexDbid);
 			if (id && dbid) {
 				cureId = parseInt(id[1]);
-				cureDbid = parseInt(dbid[1]);
+				cureDbid = BigInt(parseInt(dbid[1]));
 				command.message('Using pp consumable with id:' + cureId);
 			} else {
 				command.message('Error, not a chatLink. Please type "craft <Item>". Link the item with Ctrl+LMB.');
@@ -39,9 +39,8 @@ module.exports = function EndlessCrafting(dispatch) {
 	});
 
 	function unlock() {
-		dispatch.toClient('S_START_PRODUCE', 2, {
-			unk: false,
-   			maxProgress: 0
+		dispatch.toClient('S_START_PRODUCE', 3, {
+			duration:0
 		});
 	}
 	
@@ -49,21 +48,24 @@ module.exports = function EndlessCrafting(dispatch) {
 		gameId = event.gameId;
 	});
 	
-	dispatch.hook('S_FATIGABILITY_POINT', 2, event => {
+	dispatch.hook('S_FATIGABILITY_POINT', 3, event => {
 		pp = event.fatigability;
 	});
 	
 	dispatch.hook('C_START_PRODUCE', 1, event => {
 		craftItem = event;
 	});
-	
-	dispatch.hook('S_START_PRODUCE', 2, event => {
-		if (enabled && !event.unk && pp < 200) {
+
+	dispatch.hook('S_END_PRODUCE', 1, event => {
+		if (!enabled || !event.success) {
+			return;
+		}
+		if (pp < 500) {
 			command.message("Using pp consumable.");
 			dispatch.toServer('C_USE_ITEM', 3, {
 				gameId: gameId,
 				id: cureId,
-				dbid: {low: cureDbid, high: 0, unsigned: true},
+				dbid: cureDbid,
 				target: 0,
 				amount: 1,
 				dest: {x: 0, y: 0, z: 0},
@@ -74,15 +76,11 @@ module.exports = function EndlessCrafting(dispatch) {
 				unk3: 0,
 				unk4: true
 			});
-			dispatch.hookOnce('S_FATIGABILITY_POINT', 2, (e) => {
-				if (enabled && e.fatigability > 200)
+			dispatch.hookOnce('S_FATIGABILITY_POINT', 3, (e) => {
+				if (enabled && e.fatigability > 500)
 					dispatch.toServer('C_START_PRODUCE', 1, craftItem);
 			});
-		}
-	});
-
-	dispatch.hook('S_END_PRODUCE', 1, event => {
-		if (enabled && event.success) {
+		} else {
 			dispatch.toServer('C_START_PRODUCE', 1, craftItem);
 		}
 	});
