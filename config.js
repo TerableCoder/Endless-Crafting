2a278feacdb87745562e9bf18f2ce24f5f8a9c6d
Tera Coder <TerableCoder@gmail.com>
2019-06-28 05:34:13 -0400

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	enabled: false, // start enabled or disabled?
-    cureId: 182439, //Normal: 181100, elite: 182439
-	delay: 0, // if you have high ping and this module is failing, increase this number, 1000 = 1 second
-}
